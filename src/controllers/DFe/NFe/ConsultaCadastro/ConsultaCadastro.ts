9d076005a9ee515d460528e17bb1beb3d976a202
viniciushvsilva <43585717+viniciushvsilva@users.noreply.github.com>
2024-09-30 12:41:47 -0300

    Create ConsultaCadastro.ts

---
+++ src/controllers/DFe/NFe/ConsultaCadastro/ConsultaCadastro.ts
@@ -0,0 +1,44 @@
+import Environment from '@Classes/Environment';
+import Utility from '@Utils/Utility';
+import XmlBuilder from '@Classes/XmlBuilder';
+import BaseNFE from '../BaseNFe/BaseNFe.js';
+import { GenericObject } from '@Protocols/index.js';
+import { AxiosInstance, AxiosResponse } from 'axios';
+
+class ConsultaCadastro extends BaseNFE {
+    constructor(environment: Environment, utility: Utility, xmlBuilder: XmlBuilder, axios: AxiosInstance) {
+        super(environment, utility, xmlBuilder, 'ConsultaCadastro', axios);
+    }
+
+    protected gerarXml(chave: string): string {
+        return this.gerarXmlConsultaProtocolo(chave);
+    }
+
+    protected salvaArquivos(xmlConsulta: string, responseInJson: GenericObject, xmlRetorno: AxiosResponse<any, any>, options?: Record<string, any>): void {}
+
+    /**
+     * Método utilitário para criação do XML a partir de um Objeto
+     */
+    gerarXmlConsultaProtocolo(chave: string) {
+        try {
+            const { nfe: { ambiente, versaoDF }, dfe: { UF } } = this.environment.getConfig();
+
+            const xmlObject = {
+                $: {
+                    versao: versaoDF,
+                    xmlns: 'http://www.portalfiscal.inf.br/nfe'
+                },
+                tpAmb: ambiente,
+                xServ: 'CONSULTAR',
+                chNFe: chave,
+            }
+
+            return this.xmlBuilder.gerarXml(xmlObject, 'consSitNFe')
+        } catch (error: any) {
+            throw new Error(error.message)
+        }
+    }
+
+}
+
+export default ConsultaCadastro;
